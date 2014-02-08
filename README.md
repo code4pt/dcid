90d038de2075e17dd3a2672e86f7aaeea080ba43
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-08 15:07:04 +0000

    Add dcid (previous) assets

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 *propose . discuss . decide*
 
+[View demo](http://lit-mesa-5562.herokuapp.com/)
+
 ## Release notes
 
 ### 0.0.1
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "DCID"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  # Returns the current version of DCID
+  def app_version()
+    "0.0.1"
+  end
+
 end
