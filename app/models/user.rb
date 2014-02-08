edf2f7dd794d22608dfa0bbb16ca11b06c1d6f4a
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-08 19:59:07 +0000

    Add format validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
 
   attr_accessor :name, :email, :citizen_number   #TODO Id should be the citizen number (Long)
 
-  validates(:name, presence: true)
-  validates(:email, presence: true)
-  validates(:citizen_number, presence: true)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_CITIZEN_NUMBER_REGEX = /[1-9]\d{7,}/
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :citizen_number, presence: true, format: { with: VALID_CITIZEN_NUMBER_REGEX }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe User do
   subject { @user }
 
-  before { @user = User.new(name: "Alice Wonderland", email: "alice@wonderland.com", citizen_number:"123123123") }  
+  before { @user = User.new(name: "Alice Wonderland", email: "alice@wonderland.com",
+                            citizen_number:"123123123") }  
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
@@ -24,4 +25,51 @@ describe User do
     before { @user.citizen_number = " " }
     it { should_not be_valid }
   end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com
+                      foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when citizen number format is invalid" do
+    it "should be invalid" do
+      numbers = %w[03773392 a.word 123x567]
+      numbers.each do |invalid_numbers|
+        @user.citizen_number = invalid_numbers
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when citizen number format is valid" do
+    it "should be valid" do
+      numbers = %w[13773392]
+      numbers.each do |valid_numbers|
+        @user.citizen_number = valid_numbers
+        expect(@user).to be_valid
+      end
+    end
+  end
+
 end
