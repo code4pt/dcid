5bdf996402f6146f8bd14e8333b574b2c802bae0
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-15 19:00:18 +0000

    add has_secure_password and related tests (end 6.3.4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
 
   #attr_accessor :name, :email, :citizen_number, :password, :password_confirmation
-  attr_accessor :name, :email, :password, :password_confirmation
+  
+  has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_CITIZEN_NUMBER_REGEX = /[1-9]\d{7,}/  #TODO what is the portuguese regex for BI/CC number?
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :citizen_number, presence: true, format: { with: VALID_CITIZEN_NUMBER_REGEX }, uniqueness: true
+  validates :password, length: { minimum: 6 }
 
   before_save { lowercase_email() }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe User do
-  subject { @user }
+describe "User" do
 
+  subject { @user }
   before { @user = User.new(name: "Alice Wonderland", email: "alice@wonderland.com", citizen_number:"100100100",
                             password: "secret", password_confirmation: "secret") }
 
@@ -10,7 +10,11 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:citizen_number) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
   it { should be_valid }
+
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -27,68 +31,111 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
 
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com
-                      foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        expect(@user).not_to be_valid
+  describe "validation," do
+
+    describe "when name is too long" do
+      before { @user.name = "a" * 51 }
+      it { should_not be_valid }
+    end
+
+    describe "when email format is invalid" do
+      it "should be invalid" do
+        addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com
+                        foo@bar+baz.com]
+        addresses.each do |invalid_address|
+          @user.email = invalid_address
+          expect(@user).not_to be_valid
+        end
       end
     end
-  end
 
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        expect(@user).to be_valid
+    describe "when email format is valid" do
+      it "should be valid" do
+        addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+        addresses.each do |valid_address|
+          @user.email = valid_address
+          expect(@user).to be_valid
+        end
       end
     end
-  end
 
-  describe "when citizen number format is invalid" do
-    it "should be invalid" do
-      numbers = %w[03773392 a.word 123x567]
-      numbers.each do |invalid_numbers|
-        @user.citizen_number = invalid_numbers
-        expect(@user).not_to be_valid
+    describe "when citizen number format is invalid" do
+      it "should be invalid" do
+        numbers = %w[03773392 a.word 123x567]
+        numbers.each do |invalid_numbers|
+          @user.citizen_number = invalid_numbers
+          expect(@user).not_to be_valid
+        end
       end
     end
-  end
 
-  describe "when citizen number format is valid" do
-    it "should be valid" do
-      numbers = %w[13773392]
-      numbers.each do |valid_numbers|
-        @user.citizen_number = valid_numbers
-        expect(@user).to be_valid
+    describe "when citizen number format is valid" do
+      it "should be valid" do
+        numbers = %w[13773392]
+        numbers.each do |valid_numbers|
+          @user.citizen_number = valid_numbers
+          expect(@user).to be_valid
+        end
       end
     end
-  end
 
-  describe "when citizen number format is not valid" do
-    it "should not be valid" do
-      numbers = %w[03773392 1234 a1234567]
-      numbers.each do |valid_numbers|
-        @user.citizen_number = valid_numbers
-        expect(@user).not_to be_valid
+    describe "when citizen number format is not valid" do
+      it "should not be valid" do
+        numbers = %w[03773392 1234 a1234567]
+        numbers.each do |valid_numbers|
+          @user.citizen_number = valid_numbers
+          expect(@user).not_to be_valid
+        end
       end
     end
+
+    describe "when citizen number is already taken" do
+      before do
+        user_copy = @user.dup
+        user_copy.password_digest = "#UserPassw0rdEncrypted!"
+        user_copy.save
+      end
+      it { should_not be_valid }
+    end
   end
 
-  describe "when citizen number is already taken" do
-    before do
-      user_copy = @user.dup
-      user_copy.save
+
+  describe "authentication," do
+    
+    describe "and password is not present" do
+      before do
+        @user = User.new(name: "Example User", email: "user@example.com",
+                         password: " ", password_confirmation: " ")
+      end
+      it { should_not be_valid }
     end
-    it { should_not be_valid }
+
+    describe "and password doesn't match confirmation" do
+      before { @user.password_confirmation = "mismatch" }
+      it { should_not be_valid }
+    end
+
+    describe "and password is too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid }
+    end
+
+    describe "and return value of authenticate method," do
+      before { @user.save }
+      let(:found_user) { User.find_by(email: @user.email) }
+
+      describe "has valid password" do
+        it { should eq found_user.authenticate(@user.password) }
+      end
+
+      describe "has invalid password" do
+        let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+        it { should_not eq user_for_invalid_password }
+        specify { expect(user_for_invalid_password).to be_false }
+      end
+    end
   end
 
 end
