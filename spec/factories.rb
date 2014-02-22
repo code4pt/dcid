9e856944e706b56838502743b4cb4b6ed722438b
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-22 12:32:30 +0000

    Add tests for signup functionality

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     name           "Alice Wonderland"
     email          "alice@wonderland.com"
-    citizen_number "123123123"
+    citizen_number "100100100"
     password "secret"
     password_confirmation "secret"
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,33 @@ describe "User pages" do
 
   describe "signup page" do
     before { visit signup_path }
-
     it { should have_title(full_title('Participar')) }
     it { should have_content('Participe') }    
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Criar o meu perfil" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",           with: "Alice Wonderland"
+        fill_in "Email",          with: "alice@wonderland.com"
+        fill_in "Citizen Number", with: "100100100"
+        fill_in "Password",       with: "secret"
+        fill_in "Confirmation",   with: "secret"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
   describe "profile page" do
@@ -17,4 +41,5 @@ describe "User pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+
 end
