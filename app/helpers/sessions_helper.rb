44aeb494855f0b39e728e6b53c3a87d737163f1c
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-23 17:18:28 +0000

    Add method to return current user [8.2.3]

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,5 +6,14 @@ module SessionsHelper
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
-  
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
 end
