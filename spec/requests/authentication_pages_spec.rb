b417f724c1e4da73b7e489c614f7cd85f8c59e30
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-23 14:41:11 +0000

    Add tests for valid and invalid signin [8.1.2]

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,32 @@ describe "Authentication," do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+    it { should have_content('Entrar') }
+    it { should have_title('Entrar') }
+  end
+
+  describe "signin," do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Entrar" }
+
+      it { should have_title('Entrar') }
+      it { should have_selector('div.alert.alert-error') }
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",         with: user.email.upcase
+        fill_in "Palavra-chave", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Perfil',     href: user_path(user)) }
+      it { should have_link('Sair',    href: signout_path) }
+      it { should_not have_link('Entrar', href: signin_path) }
+    end
   end
 end
