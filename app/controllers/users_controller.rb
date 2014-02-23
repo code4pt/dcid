bb4a62c5a94e679a2645ca15b987f6965d080f65
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-23 18:06:36 +0000

    Sign in user after sign up [8.2.5]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "A partir de agora, é você que dcid."
       redirect_to @user  #user's show page
     else
