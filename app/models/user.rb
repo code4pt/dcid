05cdab3bd06967ce98e48d1c0c8817a18d2c28a2
Diogo Nunes <dialex.nunes@gmail.com>
2014-02-23 15:38:39 +0000

    Update User to generate and store the remember token [8.2.1]

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,23 @@ class User < ActiveRecord::Base
   validates :citizen_number, presence: true, format: { with: VALID_CITIZEN_NUMBER_REGEX }, uniqueness: true
   validates :password, length: { minimum: 6 }
 
-  before_save { lowercase_email() }
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token  
 
-  def lowercase_email() 
-    self.email = email.downcase
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
   end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 
 end
