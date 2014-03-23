0575c966bca828a98caedee43c12a82ac0e9ed26
Diogo Nunes <dialex.nunes@gmail.com>
2014-03-23 19:44:27 +0000

    after login redirect to home page, by default

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_back_or user
+      redirect_back_or root_path
     else
       flash.now[:error] = 'Combinação email/palavra-chave errada.'
       render 'new'
