b516fbd883760244db424833262dea0af433a5a0
Diogo Nunes <dialex.nunes@gmail.com>
2014-08-02 19:53:51 +0100

    it was not a bug, it was a feature, to prevent attackers from knowing which emails are registered

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,14 +5,10 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user
-      user.send_password_reset
-      flash.now[:success] = 'Enviado email com as instruções para recuperar a sua palavra-chave.'
-      render root_url
-    else
-      flash.now[:error] = 'De certeza que usou esse email?'
-      render 'new'
-    end
+    user.send_password_reset if user
+    flash.now[:success] = "Enviado email com as instruções para recuperar a sua palavra-chave."
+    render root_url
+    #redirect_to root_url, :notice => "Enviado email com as instruções para recuperar a sua palavra-chave."
   end
 
 end
