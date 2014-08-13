8708335d53e5e937aac13614c413e9ef2e2b9d9f
Diogo Nunes <dialex.nunes@gmail.com>
2014-08-13 16:29:59 +0100

    solve FIXME, show success when email is sent

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,9 +3,8 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
-    #flash.now[:success] = "Enviado email com as instruções para recuperar a sua palavra-chave."
-    #render root_url
-    redirect_to root_url, :notice => "Enviado email com as instruções para recuperar a sua palavra-chave."
+    flash[:success] = "Enviado email com as instruções para recuperar a sua palavra-chave."
+    redirect_to root_url
   end
 
   def edit
@@ -16,7 +15,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Expirou o tempo para recuperar a palavra-chave. Recomece o processo."
-    elsif @user.update_attributes(params[:user])
+    elsif @user.update_attributes(params.require(:user).permit(:password, :password_confirmation).merge(password_reset_token: nil))
       redirect_to sign_in, :notice => "A palavra-chave foi alterada."
     else
       render :edit
