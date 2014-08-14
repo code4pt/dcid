f0a28ca96e76dbb1eba5f9ad761dd1d5f2d8b803
Diogo Nunes <dialex.nunes@gmail.com>
2014-08-14 11:31:45 +0100

    fix bug of wrong parameters

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -15,8 +15,8 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Expirou o tempo para recuperar a palavra-chave. Recomece o processo."
-    elsif @user.update_attributes(params.require(:user).permit(:password, :password_confirmation))  #TODO .merge(password_reset_token: nil) in order to reset the token
-      redirect_to sign_in, :notice => "A palavra-chave foi alterada."
+    elsif @user.update_attributes(params.require(:user).permit(:password, :password_confirmation).merge(password_reset_token: nil))
+      redirect_to sign_in(@user), :notice => "A palavra-chave foi alterada."
     else
       render :edit
     end
